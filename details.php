4bc11abe8e00c8542dd56e0709c56b8ec0835fec
rishi <oss@rishikeshan.com>
2023-09-28 14:12:59 +0000

    Minimal working example.
    FossilOrigin-Name: 438f854ab5f50fa03337b701af445655e67c3a0b5f49a8c475ec5b109b87f823

---
+++ details.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<script>
+function goback(){window.location.assign("/scan/")}
+</script>
+<?php
+//ini_set('display_errors', '1');
+//error_reporting(E_ALL);
+require_once("/etc/auth.php");
+$ID = $_GET["id"];
+$BASEURL = "http://127.0.0.1:9090/api/Items2/";
+$req = curl_init();
+curl_setopt($req, CURLOPT_URL, "$BASEURL/$ID");
+curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($req, CURLOPT_HTTPHEADER, ["Authorization: Basic $ENCODED_AUTH"]);
+$response = json_decode(curl_exec($req));
+//var_dump($response);
+?>
+	<title>DETAILS: <?php echo $response->PLU_DESC; ?></title>
+</head>
+<body>
+<button onclick="goback()">🔙 Go back!</button>
+<table class="named">
+<tr>
+<td>Name</td>
+<td><?php echo $response->PLU_DESC; ?></td>
+</tr>
+<tr>
+<td>Code</td>
+<td><?php echo $response->PLU_CODE; ?></td>
+</tr>
+<tr>
+<td>Selling price</td>
+<td><?php echo $response->PLU_SELL; ?></td>
+</tr>
+<tr>
+<td>SIH</td>
+<td><?php echo $response->SIH; ?></td>
+</tr>
+</table>
+<table class="others">
+<tr>
+<th scope="col">Field</th>
+<th scope="col">Value</th>
+</tr>
+<?php foreach($response as $field=>$value) { ?>
+<tr>
+<td><?php echo $field; ?></td>
+<td><?php echo $value; ?></td>
+</tr>
+<?php } ?>
+</table>
+<?php
+echo "<pre>".json_encode($response, JSON_PRETTY_PRINT)."</pre>";
+?>
+</body>
+</html>
