7dd081ec274797ebf9abc54d86dcefd171a86f5e
veenu <veenupanchal241@gmail.com>
2023-10-22 15:30:36 +0000

    ss
    FossilOrigin-Name: 5a82800a6e77f60ac932b5ea58750537d058d9a34789161fcf0794186d9f5e9f

--- details.php
+++ details.php
@@ -29,7 +29,7 @@ $state_of_things="too-much";
 </head>
 <body>
 <div class ="button-container">
-<button onclick="goback()" class="btn goback" > <img  src="icons/back_button.png" style="height:2%; width:2%;"> </button>
+<button onclick="goback()" class="btn goback" > <img  src="icons/back_button.png" style="height:22%; width:22%;"> </button>
 </div>
 <table class="named">
 <tr>
