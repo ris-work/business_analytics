0a75a87bba0691ee0e515b7b911726eb8332d319
veenu <veenupanchal241@gmail.com>
2023-10-23 12:20:40 +0000

    ss
    FossilOrigin-Name: bfe3415f6bcfcff17cea289418da2570df8173bf456c7bfd28d6e640f715fb49

--- details.php
+++ details.php
@@ -31,7 +31,7 @@ $state_of_things="too-much";
 <div class ="button-container">
 <button onclick="goback()" class="btn goback" > <img  src="icons/back_button.png" style="height:42%; width:42%;"> </button>
 <div class ="button-container">
-<button class="btn graph"> <img  src="graph.png" style="height:42%; width:42%;"> </button></div>
+<button class="btn goback"> <img  src="graph.png" style="height:42%; width:42%;"> </button></div>
 </div>
 <table class="named">
 <tr>
