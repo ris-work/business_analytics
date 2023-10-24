a9ed1f50a10491509a070349beb171cfc4b90722
root <oss@rishikeshan.com>
2023-10-24 05:34:54 +0000

    Display all averages...
    FossilOrigin-Name: a829b999515a5657293908aa0d4f9ed2b7f284b5ab4aae0e291723394d1daa6b

--- details2.php
+++ details2.php
@@ -2,6 +2,7 @@
 <head>
 <link rel="stylesheet" type="text/css" href="style.css">
 <script>
+"use strict";
 function goback(){window.location.assign("/scan/")}
 </script>
 <?php
@@ -27,26 +28,51 @@ $state_of_things="too-much";
 //var_dump($response_analytics);
 $dbh = new PDO("sqlite:/saru/www-data/db.sqlite3");
 $t = $dbh->beginTransaction();
-$stmt_sql = $dbh->prepare("SELECT productsattime.TIME, s15, date as date, * FROM productsattime INNER JOIN productsattime_dailylatest ON productsattime_dailylatest.ID=productsattime.ID AND productsattime_dailylatest.latest=productsattime.TIME WHERE productsattime.ID=?");
+$stmt_sql = $dbh->prepare("SELECT productsattime.TIME, s15, s30, s60, date as date, * FROM productsattime INNER JOIN productsattime_dailylatest ON productsattime_dailylatest.ID=productsattime.ID AND productsattime_dailylatest.latest=productsattime.TIME WHERE productsattime.ID=?");
 $stmt = $stmt_sql->execute([$response->PLU_CODE]);
 $past_data=$stmt_sql->fetchAll();
 $dbh->commit();
 ?>
-<script>var past_data = JSON.parse('<?php echo json_encode($past_data); ?>')</script>
+<script>"use strict"; var past_data = JSON.parse('<?php echo json_encode($past_data); ?>')</script>
 	<title>DETAILS: <?php echo $response->PLU_DESC; ?></title>
 <script>
+"use strict";
+var avgSales15 = [];
+var avgSales30 = [];
+var avgSales60 = [];
 var deltaSales15 = [];
+var deltaSales30 = [];
+var deltaSales60 = [];
 var dates=[];
+for (var i=0; i<past_data.length-1; i++){avgSales15.push(past_data[i]['s15']/15)}
+for (var i=0; i<past_data.length-1; i++){avgSales30.push(past_data[i]['s30']/30)}
+for (var i=0; i<past_data.length-1; i++){avgSales60.push(past_data[i]['s60']/60)}
 for (var i=0; i<past_data.length-1; i++){deltaSales15.push(past_data[i]['s15']- past_data[i+1]['s15'])}
+for (var i=0; i<past_data.length-1; i++){deltaSales30.push(past_data[i]['s30']- past_data[i+1]['s30'])}
+for (var i=0; i<past_data.length-1; i++){deltaSales60.push(past_data[i]['s60']- past_data[i+1]['s60'])}
 for (var i=0; i<past_data.length; i++){dates.push(past_data[i]['date'])}
-var averageDailySalesOnDay0=past_data[0]['s15']/15;
-var averageDailySales = [averageDailySalesOnDay0];
-var c = averageDailySalesOnDay0;
+var averageDailySalesOnDay0_15=past_data[0]['s15']/15;
+var averageDailySalesOnDay0_30=past_data[0]['s30']/30;
+var averageDailySalesOnDay0_60=past_data[0]['s60']/60;
+var averageDailySales_15_d = [averageDailySalesOnDay0_15];
+var averageDailySales_30_d = [averageDailySalesOnDay0_30];
+var averageDailySales_60_d = [averageDailySalesOnDay0_60];
+var c_15 = averageDailySalesOnDay0_15;
+var c_30 = averageDailySalesOnDay0_30;
+var c_60 = averageDailySalesOnDay0_60;
 for(var i=0; i<deltaSales15.length; i++){
-	c+=deltaSales15[i];
-	averageDailySales.push(c);
+	c_15+=deltaSales15[i];
+	averageDailySales_15_d.push(c_15);
 }
-console.log(averageDailySales);
+for(var i=0; i<deltaSales30.length; i++){
+	c_30+=deltaSales30[i];
+	averageDailySales_30_d.push(c_30);
+}
+for(var i=0; i<deltaSales60.length; i++){
+	c_60+=deltaSales60[i];
+	averageDailySales_60_d.push(c_60);
+}
+console.log(averageDailySales_60_d);
 </script>
 <script type="text/javascript" src="chart.umd.min.js">
 </script>
@@ -58,7 +84,9 @@ function displayChart(){
 	type: 'line',
 		data: {
 		labels: dates,
-			datasets: [{label: "Daily Average Sales", data: averageDailySales}],
+			datasets: [{label: "Daily Average Sales (15d Average)", data: avgSales15},
+				{label: "Daily Average Sales (30d Average)", data: avgSales30},
+				{label: "Daily Average Sales (60d Average)", data: avgSales60}],
 			options: {scale: {y: {beginAtZero: true}}}
 }
 }
