f0b8f0a21e3893f631aab1de320f17a53d652f44
root <oss@rishikeshan.com>
2023-11-08 15:23:36 +0000

    Table structure for dumping daily details and also a view to view the latest information cached.
    FossilOrigin-Name: a636ce6ff009b905c310d5d0c0597bcd10d641c528f48c3a1e32737b63c38081

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 CREATE TABLE products (ID text, TIME text, SIH int, s15 int, s30 int, s60 int, SP real) strict;
 CREATE TABLE productsattime (ID text, TIME text DEFAULT CURRENT_TIMESTAMP, SIH int, s15 int, s30 int, s60 int, SP real) strict;
+CREATE TABLE productsattime_misc (ID text, TIME text DEFAULT CURRENT_TIMESTAMP, DESC text, BARCODE text, SELL real) strict;
 CREATE INDEX productsattime_daily ON productsattime (ID, substring(TIME, 0, 12));
+CREATE INDEX productsattime_idx_172303f8 ON productsattime(ID, TIME);
 CREATE VIEW productsattime_dailylatest AS SELECT productsattime.ID, substring(TIME, 0, 12) as date, max(TIME) as latest FROM productsattime GROUP BY id, substring(TIME, 0, 12)
 /* productsattime_dailylatest(ID,date,latest) */;
+CREATE VIEW productsattime_misclatest AS SELECT A.ID, productsattime_misc.TIME, DESC, SELL from productsattime_misc JOIN (select ID, max(TIME) as mt from productsattime_misc GROUP BY ID) as A ON A.ID=productsattime_misc.ID AND mt=productsattime_misc.TIME
+/* productsattime_misclatest(ID,TIME,"DESC",SELL) */;
