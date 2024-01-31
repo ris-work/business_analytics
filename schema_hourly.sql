747d6fa3d35a09b1811853cd8cc4a859c8cdcf06
root <oss@rishikeshan.com>
2024-01-31 13:21:14 +0000

    Better schema now.
    FossilOrigin-Name: 9db0b0db13b11c92e2e81b4fecab0800baacc1728485f02fed9ba9a5f9450339

--- schema_hourly.sql
+++ schema_hourly.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS "hourly"(
 "itemcode" TEXT, "daydate" TEXT, "timehour" TEXT, "quantity" TEXT);
 CREATE TABLE IF NOT EXISTS "inventory"(
 "productcode" TEXT, "productname" TEXT);
-CREATE TABLE dates(x);
 CREATE INDEX product_id ON hourly (itemcode);
 CREATE INDEX product_id_with_date ON hourly (itemcode,daydate);
 CREATE INDEX product_id_with_date_and_hour ON hourly (itemcode,daydate,timehour);
+CREATE VIEW dates as WITH RECURSIVE day(x) as (VALUES(date('2019-01-01')) UNION ALL SELECT date(x, "+1 day") FROM day WHERE x<date('now')) SELECT x from day
+/* dates(x) */;
