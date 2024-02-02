cd62375ec22dcf5982d79356897b5d1fd469d3ec
root <oss@rishikeshan.com>
2024-02-02 14:37:12 +0000

    Updated schema.
    FossilOrigin-Name: 82f89874b1e3eeea817dcacc32a108b19353acf780fc5d0b7702f94aab823350

--- schema_hourly.sql
+++ schema_hourly.sql
@@ -7,3 +7,8 @@ CREATE INDEX product_id_with_date ON hourly (itemcode,daydate);
 CREATE INDEX product_id_with_date_and_hour ON hourly (itemcode,daydate,timehour);
 CREATE VIEW dates as WITH RECURSIVE day(x) as (VALUES(date('2019-01-01')) UNION ALL SELECT date(x, "+1 day") FROM day WHERE x<date('now')) SELECT x from day
 /* dates(x) */;
+CREATE VIEW cnt AS WITH RECURSIVE cnta(x) as (SELECT 0 UNION ALL SELECT x+1 FROM cnta WHERE x < 1000) SELECT x FROM cnta
+/* cnt(x) */;
+CREATE INDEX updated_dates ON hourly(daydate);
+CREATE VIEW last_imported AS SELECT max(daydate) FROM hourly
+/* last_imported("max(daydate)") */;
