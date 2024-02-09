1ff61a5cd24da592395ddd29183538b29d638797
root <oss@rishikeshan.com>
2024-02-09 17:54:11 +0000

    Primary key, help with continuous imports...
    FossilOrigin-Name: 1190184876a9636acde9c2813133cc69b597557a56e5a0f19994f6df78c5e975

--- schema_hourly.sql
+++ schema_hourly.sql
@@ -1,14 +1,15 @@
-CREATE TABLE IF NOT EXISTS "hourly"(
-"itemcode" TEXT, "daydate" TEXT, "timehour" TEXT, "quantity" TEXT);
 CREATE TABLE IF NOT EXISTS "inventory"(
 "productcode" TEXT, "productname" TEXT);
-CREATE INDEX product_id ON hourly (itemcode);
-CREATE INDEX product_id_with_date ON hourly (itemcode,daydate);
-CREATE INDEX product_id_with_date_and_hour ON hourly (itemcode,daydate,timehour);
+CREATE TABLE hourly ("itemcode" TEXT, "daydate" TEXT, "timehour" TEXT, "quantity" TEXT, PRIMARY KEY (itemcode, daydate, timehour));
+CREATE TABLE hourly_changes('DATETIME', 'itemcode', 'daydate', 'timehour', 'qty', 'qty_change');
 CREATE VIEW dates as WITH RECURSIVE day(x) as (VALUES(date('2019-01-01')) UNION ALL SELECT date(x, "+1 day") FROM day WHERE x<date('now')) SELECT x from day
 /* dates(x) */;
 CREATE VIEW cnt AS WITH RECURSIVE cnta(x) as (SELECT 0 UNION ALL SELECT x+1 FROM cnta WHERE x < 1000) SELECT x FROM cnta
 /* cnt(x) */;
-CREATE INDEX updated_dates ON hourly(daydate);
 CREATE VIEW last_imported AS SELECT max(daydate) FROM hourly
 /* last_imported("max(daydate)") */;
+CREATE TRIGGER hourly_changes_logger BEFORE UPDATE ON hourly FOR EACH ROW BEGIN INSERT INTO hourly_changes VALUES (date()||'T'||time(), OLD.itemcode, OLD.daydate, OLD.timehour, OLD.qty, OLD.qty - NEW.qty); END;
+CREATE TABLE hourly_import ('itemcode', 'daydate', 'timehour', 'qty');
+CREATE INDEX product_id ON hourly (itemcode);
+CREATE INDEX product_id_with_date ON hourly (itemcode,daydate);
+CREATE INDEX product_id_with_date_and_hour ON hourly (itemcode,daydate,timehour);
