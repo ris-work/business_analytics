4fceabe27e9086581b9df4cbc25c00f73b49b872
root <oss@rishikeshan.com>
2024-02-11 01:29:15 +0000

    Create a view to look for zeroed-out entries on import, to set them to zero with an update.
    FossilOrigin-Name: 6833606f906eef50c3dbbdaa6996aac5c96c64b55104661804ab07d6b2a7ec20

--- queries/sample.ps1
+++ queries/sample.ps1
@@ -1,5 +1,5 @@
 . /saru/auth.ps1
 $query = Get-Content query_hourly.sql
 Get-Date -Format "o"
-Invoke-Sqlcmd -ServerInstance "127.0.0.1,21433" -Query "$query" -Encrypt "Optional" -TrustServerCertificate -User "pos" -Password "$cred" | ConvertTo-csv -NoHeader | Out-File -File sample.csv
+Invoke-Sqlcmd -ServerInstance "127.0.0.1,21433" -Query "$query" -Encrypt "Optional" -TrustServerCertificate -User "pos" -Password "$cred" | ConvertTo-csv -NoHeader | Out-File -File sample.csv.inprogress && mv sample.csv.inprogress sample.csv
 Get-Date -Format "o"
--- schema_hourly.sql
+++ schema_hourly.sql
@@ -3,10 +3,12 @@ CREATE TABLE IF NOT EXISTS "inventory"(
 CREATE TABLE hourly ("itemcode" TEXT, "daydate" TEXT, "timehour" TEXT, "quantity" TEXT, PRIMARY KEY (itemcode, daydate, timehour));
 CREATE TABLE hourly_changes('DATETIME', 'itemcode', 'daydate', 'timehour', 'qty', 'qty_change');
 CREATE TABLE hourly_import ('itemcode', 'daydate', 'timehour', "quantity");
+CREATE TABLE sqlite_stat1(tbl,idx,stat);
 CREATE INDEX product_id ON hourly (itemcode);
 CREATE INDEX product_id_with_date ON hourly (itemcode,daydate);
 CREATE INDEX product_id_with_date_and_hour ON hourly (itemcode,daydate,timehour);
 CREATE INDEX updated_dates ON hourly(daydate);
+CREATE INDEX hourly_import_comparison ON hourly_import(itemcode, daydate, timehour);
 CREATE VIEW dates as WITH RECURSIVE day(x) as (VALUES(date('2019-01-01')) UNION ALL SELECT date(x, '+1 day') FROM day WHERE x<date('now')) SELECT x from day
 /* dates(x) */;
 CREATE VIEW cnt AS WITH RECURSIVE cnta(x) as (SELECT 0 UNION ALL SELECT x+1 FROM cnta WHERE x < 1000) SELECT x FROM cnta
@@ -14,3 +16,9 @@ CREATE VIEW cnt AS WITH RECURSIVE cnta(x) as (SELECT 0 UNION ALL SELECT x+1 FROM
 CREATE VIEW last_imported AS SELECT max(daydate) FROM hourly
 /* last_imported("max(daydate)") */;
 CREATE TRIGGER hourly_changes_logger BEFORE UPDATE ON hourly FOR EACH ROW BEGIN INSERT INTO hourly_changes VALUES (date()||'T'||time(), OLD.itemcode, OLD.daydate, OLD.timehour, OLD.quantity, OLD.quantity - NEW.quantity); END;
+CREATE VIEW hourly_existing_entries AS SELECT itemcode, daydate, timehour FROM hourly
+/* hourly_existing_entries(itemcode,daydate,timehour) */;
+CREATE VIEW hourly_import_existing_entries AS SELECT itemcode, daydate, timehour FROM hourly_import
+/* hourly_import_existing_entries(itemcode,daydate,timehour) */;
+CREATE VIEW zeroed_out_on_import AS SELECT itemcode, daydate, timehour FROM hourly EXCEPT SELECT itemcode, daydate, timehour FROM hourly_import
+/* zeroed_out_on_import(itemcode,daydate,timehour) */;
