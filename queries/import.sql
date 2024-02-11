ea80a92a7abd3d59762d8231b069ea3eb399eb5b
root <oss@rishikeshan.com>
2024-02-11 03:17:51 +0000

    Zeroed out values...
    FossilOrigin-Name: e7ad1df301bd93257c7786b336fcf3a08677471456c2ed207d71f1df0b647a7d

--- queries/import.sql
+++ queries/import.sql
@@ -2,4 +2,8 @@ BEGIN TRANSACTION;
 DELETE FROM hourly_import;
 .import --csv sample.csv hourly_import
 INSERT INTO hourly SELECT * FROM hourly_import WHERE true ON CONFLICT DO UPDATE SET quantity=excluded.quantity WHERE quantity <> excluded.quantity;
+UPDATE hourly SET quantity=0 WHERE (itemcode, daydate, timehour) IN zeroed_out_on_import;
+DELETE FROM hourly_import;
 COMMIT;
+VACUUM;
+ANALYZE;
