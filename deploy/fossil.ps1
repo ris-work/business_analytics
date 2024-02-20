c680c10fb43da17c8552603b1dd3c4fd48aab25c
root <oss@rishikeshan.com>
2024-02-20 12:17:27 +0000

    Copy cacert.pem (deploy)
    FossilOrigin-Name: ce7d854860b3f9fad72b70a733031db62f1669bebe65c1671c54619b6778aba7

--- deploy/fossil.ps1
+++ deploy/fossil.ps1
@@ -1,4 +1,5 @@
 curl.exe -O "https://fossil-scm.org/home/uv/fossil-w64-2.23.zip"
 Expand-Archive "./fossil-w64-2.23.zip"
-copy fossil.exe c:\windows\
+copy fossil.exe "c:\windows\"
 curl.exe -O "https://curl.se/ca/cacert.pem"
+copy cacert.pem "c:\windows\"
