875472479be7ee975d41d0a994e2f652b50c902c
root <oss@rishikeshan.com>
2024-03-25 11:30:25 +0000

    Add cost loader
    FossilOrigin-Name: f95b4a6130a21a23129d07100fd1cddcbf4eb92d092acdc5d979f04a65d588ac

---
+++ cost/cost.sql
@@ -0,0 +1,9 @@
+BEGIN TRANSACTION;
+DELETE FROM cost_import;
+.import --csv cost.csv cost_import
+INSERT INTO cost SELECT * FROM cost_import WHERE true ON CONFLICT DO UPDATE SET cost=excluded.cost WHERE cost <> excluded.cost;
+--UPDATE cost SET quantity=0 WHERE (itemcode, daydate, timehour) IN zeroed_out_on_import;
+DELETE FROM cost_import;
+COMMIT;
+VACUUM;
+ANALYZE;
