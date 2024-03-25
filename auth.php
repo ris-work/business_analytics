beed98857181b1313060fc29187d9f1e37e64c55
root <oss@rishikeshan.com>
2024-03-25 11:38:17 +0000

    Common variables.
    FossilOrigin-Name: d51e4cad2f24f9d85740fd4576df05f654865b8390bb9824ebb1792ab8998a6d

---
+++ auth.php
@@ -0,0 +1,4 @@
+<?php
+$cred="";
+$serv="127.0.0.1,port"; // NOTE THE COMMA, DON'T USE COLON
+?>
--- queries/details.ps1
+++ queries/details.ps1
@@ -9,7 +9,7 @@ foreach($table in $tables){
 #Invoke-Sqlcmd -ServerInstance "127.0.0.1,21433" -Query "$query" -Encrypt "Optional" -TrustServerCertificate -User "pos" -Password "$cred" | ConvertTo-csv -NoHeader | Out-File -File sih.csv.inprogress && mv sih.csv.inprogress sih.csv
 #Invoke-Sqlcmd -ServerInstance "127.0.0.1,21433" -Query "$query_t" -Encrypt "Optional" -TrustServerCertificate -User "pos" -Password "$cred" | ConvertTo-csv | Out-File -File sih_t.csv.inprogress && mv sih_t.csv.inprogress sih_t.csv
 $name = $table.TABLE_NAME
-Invoke-Sqlcmd -ServerInstance "127.0.0.1,21433" -Query "SELECT count(*), `'$name`' FROM $name" -Encrypt "Optional" -TrustServerCertificate -User "pos" -Password "$cred" | ConvertTo-csv -NoHeader | Out-File -Append -File size.csv.inprogress 
+Invoke-Sqlcmd -ServerInstance "$serv" -Query "SELECT count(*), `'$name`' FROM $name" -Encrypt "Optional" -TrustServerCertificate -User "pos" -Password "$cred" | ConvertTo-csv -NoHeader | Out-File -Append -File size.csv.inprogress 
 Get-Date -Format "o"
 }
 mv size.csv.inprogress size.csv
