7fd88f47848608e0a0234a1722a82bb35ef6e2a0
root <oss@rishikeshan.com>
2024-04-19 07:58:41 +0000

    Try to load locally for a special local page...
    FossilOrigin-Name: bfc740694c86690c6fd348bbd4423c15658468b6bf7d7abf26c215dfb2060719

---
+++ ql_info.php
@@ -0,0 +1,26 @@
+<?php
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+//var_dump($_POST["ids"]);
+//var_dump($_POST);
+//$IDs = json_decode($_POST["ids"]);
+$IDs = json_decode($_GET["ids"]);
+//var_dump($IDs);
+$REQUESTS = [];
+$RESPONSES=[];
+$dbh = new PDO("sqlite:/saru/www-data/hourly.sqlite3");
+foreach($IDs as $ID){
+var_dump($IDs);
+//$dbh = new PDO("sqlite:/saru/www-data/hourly.sqlite3");
+$t = $dbh->beginTransaction();
+$stmt_sql = $dbh->prepare("SELECT * FROM (SELECT sum(quantity) as S90, itemcode FROM hourly WHERE itemcode IN (?) AND daydate BETWEEN date('now', '-91 days') AND date('now', '-1 day')) S90T NATURAL INNER JOIN (SELECT sum(quantity) AS S45 FROM hourly WHERE itemcode IN (?) AND daydate BETWEEN date('now', '-46 days') AND date('now', '-1 day')) S45T NATURAL INNER JOIN (SELECT sum(quantity) AS S30 FROM hourly WHERE itemcode IN (?) AND daydate BETWEEN date('now', '-31 days') AND date('now', '-1 day')) AS S30T");
+$stmt = $stmt_sql->execute([$ID, $ID, $ID]);
+$past_data=$stmt_sql->fetchAll();
+$dbh->commit();
+var_dump($past_data);
+array_push($RESPONSES, $past_data);
+}
+echo json_encode($RESPONSES);
+//$response = json_decode(curl_exec($req));
+//var_dump($response);
+?>
