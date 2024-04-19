536784a729b4367392760670f494f207897c23b8
root <oss@rishikeshan.com>
2024-04-19 09:27:25 +0000

    Let the keys and days correspond...
    FossilOrigin-Name: 74c2f271fe5d7ff24a03cda7f85c3df4d509980169e8e636d9ecbfe0936c374c

--- ql_info.php
+++ ql_info.php
@@ -13,7 +13,7 @@ foreach($IDs as $ID){
 //var_dump($IDs);
 //$dbh = new PDO("sqlite:/saru/www-data/hourly.sqlite3");
 $t = $dbh->beginTransaction();
-$stmt_sql = $dbh->prepare("SELECT * FROM (SELECT sum(quantity) as S90, itemcode FROM hourly WHERE itemcode IN (?) AND daydate BETWEEN date('now', '-91 days') AND date('now', '-1 day')) S90T NATURAL INNER JOIN (SELECT sum(quantity) AS S45 FROM hourly WHERE itemcode IN (?) AND daydate BETWEEN date('now', '-46 days') AND date('now', '-1 day')) S45T NATURAL INNER JOIN (SELECT sum(quantity) AS S30 FROM hourly WHERE itemcode IN (?) AND daydate BETWEEN date('now', '-31 days') AND date('now', '-1 day')) AS S30T");
+$stmt_sql = $dbh->prepare("SELECT * FROM (SELECT sum(quantity) as S_D60, itemcode FROM hourly WHERE itemcode IN (?) AND daydate BETWEEN date('now', '-61 days') AND date('now', '-1 day')) S60T NATURAL INNER JOIN (SELECT sum(quantity) AS S_D30 FROM hourly WHERE itemcode IN (?) AND daydate BETWEEN date('now', '-31 days') AND date('now', '-1 day')) S30T NATURAL INNER JOIN (SELECT sum(quantity) AS S_D15 FROM hourly WHERE itemcode IN (?) AND daydate BETWEEN date('now', '-16 days') AND date('now', '-1 day')) AS S15T");
 $stmt = $stmt_sql->execute([$ID, $ID, $ID]);
 $past_data=$stmt_sql->fetchAll();
 $dbh->commit();
