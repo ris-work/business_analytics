bff8cbeb1a7d7a46c9caedc8a12d78a2e992823e
root <oss@rishikeshan.com>
2024-04-20 12:08:06 +0000

    It works, revert to working version...
    FossilOrigin-Name: e52f68e639aaf21c47d665ec13bc4cb3d2628a00004e75062d673eaf4ca562c9

--- schema_hourly.sql
+++ schema_hourly.sql
@@ -4,11 +4,21 @@ CREATE TABLE hourly ("itemcode" TEXT, "daydate" TEXT, "timehour" TEXT, "quantity
 CREATE TABLE hourly_changes('DATETIME', 'itemcode', 'daydate', 'timehour', 'qty', 'qty_change');
 CREATE TABLE hourly_import ('itemcode', 'daydate', 'timehour', "quantity");
 CREATE TABLE sqlite_stat1(tbl,idx,stat);
+CREATE TABLE cost(itemcode TEXT, daydate TEXT, cost TEXT, PRIMARY KEY (itemcode, daydate));
+CREATE TABLE cost_import(itemcode TEXT, daydate TEXT, cost TEXT);
+CREATE TABLE cost_purchase(itemcode TEXT, runno TEXT, date TEXT, cost TEXT, PRIMARY KEY (itemcode));
+CREATE TABLE IF NOT EXISTS "cost_purchase_import"(
+"itemcode" TEXT, "RUNNO" TEXT, "date" TEXT, "cost" TEXT);
+CREATE TABLE sih_current(itemcode TEXT, desc TEXT, sih TEXT, cost TEXT, sell TEXT, PRIMARY KEY (itemcode));
+CREATE TABLE IF NOT EXISTS "sih_import"(
+"PLU_CODE" TEXT, "PLU_DESC" TEXT, "SIH" TEXT, "COSTVALUE" TEXT,
+ "SELLVALUE" TEXT);
 CREATE INDEX product_id ON hourly (itemcode);
 CREATE INDEX product_id_with_date ON hourly (itemcode,daydate);
 CREATE INDEX product_id_with_date_and_hour ON hourly (itemcode,daydate,timehour);
 CREATE INDEX updated_dates ON hourly(daydate);
 CREATE INDEX hourly_import_comparison ON hourly_import(itemcode, daydate, timehour);
+CREATE INDEX cost_purchase_itemcode ON cost_purchase(itemcode);
 CREATE VIEW dates as WITH RECURSIVE day(x) as (VALUES(date('2019-01-01')) UNION ALL SELECT date(x, '+1 day') FROM day WHERE x<date('now')) SELECT x from day
 /* dates(x) */;
 CREATE VIEW cnt AS WITH RECURSIVE cnta(x) as (SELECT 0 UNION ALL SELECT x+1 FROM cnta WHERE x < 1000) SELECT x FROM cnta
