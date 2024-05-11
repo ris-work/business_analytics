b9218b07e2472ae1b02671e4606a101d6afafd77
root <oss@rishikeshan.com>
2024-05-11 14:07:03 +0000

    Formatting, it looks good now\!
    FossilOrigin-Name: e4d14b685ec0889f8efc31f088042e47ed2ddff447225a3e26a95a277205e2ec

--- reports/nolongerinstock.php
+++ reports/nolongerinstock.php
@@ -3,14 +3,18 @@
 font-family: "IBM Courier";
 src: local('IBM Courier'), url("font-og-courier/fonts/OGCourier-Bold.otf") format("opentype");
 }
+@font-face{
+font-family: "Cousine";
+src: local('Cousine'), url("fonts/apache/cousine/Cousine-Regular.ttf") format("truetype");
+}
 </style>
-<pre style="font-family: 'IBM Courier', 'Consolas', 'Lucida', MONOSPACE; font-weight: 500;">
+<pre style="font-family: 'Cousine', 'IBM Courier', 'Consolas', 'Lucida', MONOSPACE; font-weight: 500; color: indigo">
 <?php
 error_reporting(E_ALL);
 require_once "./env.php";
 $arg_dbname = escapeshellarg($dbname);
 $arg_scriptname = escapeshellarg(".read " . __FILE__.".sql");
-echo "Running: ". ("/usr/bin/sqlite3 $arg_dbname $arg_scriptname") . "\n";
+echo "Run: ". ("/usr/bin/sqlite3 $arg_dbname $arg_scriptname") . "\n";
 echo shell_exec("/usr/bin/sqlite3 $arg_dbname $arg_scriptname");
 ?>
 </pre>
--- reports/nolongerinstock.php.sql
+++ reports/nolongerinstock.php.sql
@@ -3,5 +3,8 @@
 .header on
 .changes on
 .timer on
-SELECT printf('%12.2f', gp) as gp, printf("%12.2f", sp) as sp, printf("%12.2f", cp) as cp, desc, printf("%5.f", sih) AS sih FROM (SELECT tr_p.sp, tr_p.gp, tr_p.cp, sih_current.desc, sih_current.sih FROM (SELECT sum(sumsell-sumcost) AS gp, sum(sumsell) AS sp, sum(sumcost) AS cp , itemcode from tentative_revenue GROUP BY itemcode) tr_p LEFT JOIN sih_current ON sih_current.itemcode=tr_p.itemcode WHERE sih IS NOT NULL ORDER BY sih ASC, gp DESC LIMIT 100);
+.echo off
+.print "<pre style='text-align: center; font-family: \"Cousine\", \"IBM Courier\"; color: black'>"
+SELECT printf('%12.2f', gp) as gp, printf("%12.2f", sp) as sp, printf("%12.2f", cp) as cp, desc, printf("%5.f", sih) AS sih FROM (SELECT tr_p.sp, tr_p.gp, tr_p.cp, sih_current.desc, CAST (sih_current.sih AS REAL) AS sih FROM (SELECT sum(sumsell-sumcost) AS gp, sum(sumsell) AS sp, sum(sumcost) AS cp , itemcode from tentative_revenue GROUP BY itemcode) tr_p LEFT JOIN sih_current ON sih_current.itemcode=tr_p.itemcode WHERE sih IS NOT NULL ORDER BY sih ASC, gp DESC LIMIT 200);
+.print "</pre>"
 .stats
