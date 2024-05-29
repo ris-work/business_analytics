7f8000b45be4a011c934423cda48d97933852dc7
root <oss@rishikeshan.com>
2024-05-29 10:52:40 +0000

    Fix cost import script from purchases; it was a datatype issue: '9'>'10'
    FossilOrigin-Name: 24f2fcfcfa0181f7bf3975376a956e054c58d3064e13aeac737a858c92410e9e

--- cost_purchase/cost.sql
+++ cost_purchase/cost.sql
@@ -1,7 +1,7 @@
 BEGIN TRANSACTION;
 DELETE FROM cost_purchase_import;
 .import --csv cost.csv cost_purchase_import
-INSERT INTO cost_purchase SELECT * FROM cost_purchase_import WHERE true ON CONFLICT DO UPDATE SET SIH=excluded.SIH WHERE SIH <> excluded.SIH;
+INSERT INTO cost_purchase SELECT * FROM cost_purchase_import WHERE true ON CONFLICT DO UPDATE SET RUNNO=excluded.RUNNO, date = excluded.date, cost = excluded.cost WHERE true; --WHERE SIH <> excluded.SIH;
 --UPDATE cost SET quantity=0 WHERE (itemcode, daydate, timehour) IN zeroed_out_on_import;
 DELETE FROM cost_purchase_import;
 COMMIT;
