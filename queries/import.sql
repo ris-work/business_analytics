797f32c7ffe117199a3138db9937789cbceaa265
root <oss@rishikeshan.com>
2024-05-31 13:14:04 +0000

    Fix missing entries...
    FossilOrigin-Name: 8fc664f942ece45ccc7e255e8f4a229e618b3a34615596f40587ca280517a02b

--- queries/import.sql
+++ queries/import.sql
@@ -1,7 +1,7 @@
 BEGIN TRANSACTION;
 DELETE FROM hourly_import;
 .import --csv sample.csv hourly_import
-INSERT INTO hourly SELECT * FROM hourly_import WHERE true ON CONFLICT DO UPDATE SET quantity=excluded.quantity WHERE quantity <> excluded.quantity;
+INSERT INTO hourly SELECT * FROM hourly_import WHERE true ON CONFLICT DO UPDATE SET quantity=excluded.quantity, sumsell=excluded.sumsell, sumcost=excluded.sumcost WHERE quantity <> excluded.quantity OR sumsell <> excluded.sumsell OR sumcost <> excluded.sumcost;
 --UPDATE hourly SET quantity=0 WHERE (itemcode, daydate, timehour) IN zeroed_out_on_import;
 DELETE FROM hourly_import;
 COMMIT;
