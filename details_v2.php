33df985eae158b4d2b0725602ba04c30e3b3b634
root <oss@rishikeshan.com>
2024-06-19 15:09:51 +0000

    Make details_v2 work locally
    FossilOrigin-Name: e94836f91fde9f2de93e188d9109932aff898e1a0065815d218e42b0ea6fd5c8

--- details_v2.php
+++ details_v2.php
@@ -16,7 +16,7 @@ $req = curl_init();
 curl_setopt($req, CURLOPT_URL, "$BASEURL/$ID");
 curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($req, CURLOPT_HTTPHEADER, ["Authorization: Basic $ENCODED_AUTH"]);
-$response = json_decode(curl_exec($req));
+//$response = json_decode(curl_exec($req));
 //var_dump($response);
 if (true || $response && !property_exists($response, "Message")){
 $BASEURL_ANALYTICS = "http://127.0.0.1:9090/api/Items2/GetSalesDataForAnalysis";
@@ -25,7 +25,7 @@ curl_setopt($req_analytics, CURLOPT_URL, "$BASEURL_ANALYTICS?PLU_CODE=$response-
 curl_setopt($req_analytics, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($req_analytics, CURLOPT_HTTPHEADER, ["Authorization: Basic $ENCODED_AUTH"]);
 if($response==null){$response = new class{public $PLU_CODE = "";}; $response->PLU_CODE=$_GET["id"];};
-$response_analytics = json_decode(curl_exec($req_analytics));
+//$response_analytics = json_decode(curl_exec($req_analytics));
 $SIH = $response_analytics->SIH;
 $state_of_things="too-much";
 //var_dump($response);
@@ -46,14 +46,16 @@ $data_cost=$stmt_sql_cost->fetchAll();
 $dbh_cost->commit();
 $dbh_cost_grn = new PDO("sqlite:/saru/www-data/hourly.sqlite3");
 $t_cost_grn = $dbh_cost_grn->beginTransaction();
-$stmt_sql_cost_grn = $dbh_cost_grn->prepare("SELECT itemcode, date, cost, runno FROM cost_purchase WHERE itemcode=?");
+$stmt_sql_cost_grn = $dbh_cost_grn->prepare("SELECT cost_purchase.itemcode, date, cost_purchase.cost, runno, desc, sih FROM cost_purchase LEFT JOIN sih_current ON sih_current.itemcode=cost_purchase.itemcode WHERE cost_purchase.itemcode=?");
 $stmt_cost_grn = $stmt_sql_cost_grn->execute([$response->PLU_CODE]);
 $data_cost_grn=$stmt_sql_cost_grn->fetchAll();
 $dbh_cost_grn->commit();
+$last = $past_data[count($past_data)-1];
+//var_dump($last);
 ?>
 <script>"use strict"; var past_data = JSON.parse('<?php echo json_encode($past_data); ?>')</script>
 <script>"use strict"; var data_cost = JSON.parse('<?php echo json_encode($data_cost); ?>')</script>
-	<title>DETAILS: <?php echo $response->PLU_DESC; ?></title>
+	<title>DETAILS: <?php echo $data_cost_grn[0]["desc"]; ?></title>
 <script>
 "use strict";
 var avgSales15 = [];
@@ -149,39 +151,40 @@ function displayChart(){
 <table class="named">
 <tr>
 <th>Name</th>
-<td><?php echo $response->PLU_DESC; ?></td>
+<td><?php echo $data_cost_grn[0]["desc"]; ?></td>
 </tr>
 <tr>
 <th>Code</th>
 <td><?php echo $response->PLU_CODE; ?></td>
 </tr>
 <tr>
-<th>Selling price</th>
-<td><?php echo $response->PLU_SELL; ?></td>
+<th>Sold price</th>
+<td><?php echo $last["avgsell"]; ?></td>
 </tr>
 <tr class="<?php echo $state_of_things; ?>">
 <th>SIH</th>
-<td><?php echo $response->SIH; ?></td>
+<!--<td><?php echo $response->SIH; ?></td>-->
+<td><?php echo $data_cost_grn[0]["sih"]; ?></td>
 </tr>
 <tr>
 <th>Fill sold (15 days)</th>
-<td><?php echo $response_analytics->S_D15 - $response->SIH; ?></td>
+<td><?php echo $last["s15"] - $last["SIH"]; ?></td>
 </tr>
 <tr>
 <th>Fill sold (30 days)</th>
-<td><?php echo $response_analytics->S_D30 - $response->SIH; ?></td>
+<td><?php echo $last["s30"] - $last["SIH"]; ?></td>
 </tr>
 <tr>
 <th>Fill sold (60 days)</th>
-<td><?php echo $response_analytics->S_D60 - $response->SIH; ?></td>
+<td><?php echo $last["s60"] - $last["SIH"]; ?></td>
 </tr>
 <tr>
 <th>Cost<sup> (<?php echo $data_cost[0]["daydate"]; ?>)</sup><sup style="font-size: 0.25em">Average of entries</sup></th>
-<td><?php echo number_format($data_cost[0]["cost"], 2); ?><sup> gross <?php echo number_format(100*$response->PLU_SELL/$data_cost[0]["cost"] - 100, 2); ?>%</sup></td>
+<td><?php echo number_format($data_cost[0]["cost"], 2); ?><sup> gross <?php echo number_format(100*$last["avgsell"]/$data_cost[0]["cost"] - 100, 2); ?>%</sup></td>
 </tr>
 <tr>
 <th>Cost<sup> (<?php echo substr($data_cost_grn[0]["date"], 0, 10); ?>)</sup><sup style="font-size: 0.25em">GRN (<?php echo $data_cost_grn[0]["runno"]; ?>)</sup></th>
-<td><?php echo number_format($data_cost_grn[0]["cost"], 2); ?><sup> gross <?php echo number_format(100*$response->PLU_SELL/$data_cost_grn[0]["cost"] - 100, 2); ?>%</sup></td>
+<td><?php echo number_format($data_cost_grn[0]["cost"], 2); ?><sup> gross <?php echo number_format(100*$last["avgsell"]/$data_cost_grn[0]["cost"] - 100, 2); ?>%</sup></td>
 </tr>
 <tr>
 <td>Sells every:</td>
@@ -204,7 +207,7 @@ function displayChart(){
 <td><?php echo $value; ?></td>
 </tr>
 <?php } ?>
-<?php foreach($response_analytics as $field=>$value) { ?>
+<?php foreach($last as $field=>$value) { ?>
 <tr>
 <td><?php echo $field; ?></td>
 <td><?php echo $value; ?></td>
@@ -213,7 +216,7 @@ function displayChart(){
 </table>
 <?php
 echo "<pre>".json_encode($response, JSON_PRETTY_PRINT)."</pre>";
-echo "<pre>".json_encode($response_analytics, JSON_PRETTY_PRINT)."</pre>";
+echo "<pre>".json_encode($last, JSON_PRETTY_PRINT)."</pre>";
 }
 else if($response==null){
 ?>
