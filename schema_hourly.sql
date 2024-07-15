9e2df9fe2ee91eee60b762e5633741fa2ea46606
root <oss@rishikeshan.com>
2024-07-15 16:00:47 +0000

    Tidify schema
    FossilOrigin-Name: 69d9531a22bb8c6dd90bc7ddd047b358a12cab98432481c63fdfab36642c6b00

--- schema_hourly.sql
+++ schema_hourly.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS "sih_import"(
  "SELLVALUE" TEXT);
 CREATE TABLE selling(itemcode TEXT, sell TEXT, PRIMARY KEY (itemcode));
 CREATE TABLE IF NOT EXISTS "selling_import"(
-"code" TEXT, "Column1" TEXT);
+"code" TEXT, "sell" TEXT);
 CREATE TABLE prod_list(dest TEXT NOT NULL, src TEXT NOT NULL, cost_src TEXT NOT NULL, proportion TEXT NOT NULL, PRIMARY KEY (dest, src)) STRICT;
 CREATE TABLE IF NOT EXISTS "prod_list_import"(
 "dest" TEXT, "src" TEXT, "cost_src" TEXT, "proportion" TEXT);
@@ -19,8 +19,8 @@ CREATE TABLE inventory(itemcode TEXT NOT NULL, productname TEXT, PRIMARY KEY (it
 CREATE TABLE IF NOT EXISTS "inventory_import"(
 "itemcode" TEXT, "productname" TEXT);
 CREATE TABLE IF NOT EXISTS "hourly_import"(
-"000001" TEXT, "2021-08-07" TEXT, "10" TEXT, "20.000" TEXT,
- "960.00" TEXT, "834.80000" TEXT);
+"itemcode" TEXT, "daydate" TEXT, "timehour" TEXT, "quantity" TEXT,
+ "sumsell" TEXT, "sumcost" TEXT);
 CREATE TABLE IF NOT EXISTS "tentative_revenue_import"(
 "productcode" TEXT, "daydate" TEXT, "timehour" TEXT, "qty" TEXT,
  "sumsell" TEXT, "sumcost" TEXT);
@@ -31,6 +31,15 @@ CREATE TABLE IF NOT EXISTS "full_inventory_current_import"(
 CREATE TABLE full_inventory_current (itemcode INT, sell REAL, cost REAL, PRIMARY KEY (itemcode)) STRICT;
 CREATE TABLE sih_history(datetime TEXT NOT NULL, itemcode INT NOT NULL, desc TEXT, sih INT, cost REAL, sell REAL, PRIMARY KEY (datetime, itemcode)) STRICT;
 CREATE TABLE full_inventory_history (datetime TEXT NOT NULL, itemcode INT NOT NULL, sell REAL, cost REAL, primary key(datetime, itemcode)) STRICT;
+CREATE TABLE sih_history_desc(datetime TEXT, itemcode INT, desc TEXT) STRICT;
+CREATE TABLE prod_list_history(
+  what_happened,
+  time,
+  dest TEXT,
+  src TEXT,
+  cost_src TEXT,
+  proportion TEXT
+);
 CREATE INDEX cost_purchase_itemcode ON cost_purchase(itemcode);
 CREATE INDEX sih_current_description_sih ON sih_current(itemcode, desc, sih);
 CREATE INDEX tentative_revenue_everything ON tentative_revenue(itemcode, daydate, timehour, sumsell, sumcost);
@@ -56,16 +65,13 @@ CREATE VIEW last_imported AS SELECT max(daydate) FROM hourly
 /* last_imported("max(daydate)") */;
 CREATE VIEW hourly_existing_entries AS SELECT itemcode, daydate, timehour FROM hourly
 /* hourly_existing_entries(itemcode,daydate,timehour) */;
-CREATE VIEW hourly_import_existing_entries AS SELECT itemcode, daydate, timehour FROM hourly_import;
-CREATE VIEW zeroed_out_on_import AS SELECT itemcode, daydate, timehour FROM hourly EXCEPT SELECT itemcode, daydate, timehour FROM hourly_import;
 CREATE VIEW everything_itemcode_in_hourly AS SELECT DISTINCT itemcode FROM hourly
 /* everything_itemcode_in_hourly(itemcode) */;
 CREATE VIEW t_sumrev AS SELECT itemcode, sum(sumsell) AS cumulativesell, sum(sumcost) AS cumulativecost FROM hourly GROUP BY itemcode
 /* t_sumrev(itemcode,cumulativesell,cumulativecost) */;
 CREATE TRIGGER hourly_changes_logger BEFORE UPDATE ON hourly FOR EACH ROW BEGIN INSERT INTO hourly_changes VALUES (date()||'T'||time(), OLD.itemcode, OLD.daydate, OLD.timehour, OLD.quantity, OLD.quantity - NEW.quantity); END;
 CREATE TRIGGER full_inventory_history_logger AFTER UPDATE ON full_inventory_current FOR EACH ROW WHEN OLD.sell <> NEW.sell OR OLD.cost <> NEW.cost BEGIN INSERT INTO full_inventory_history VALUES (datetime(), OLD.itemcode, OLD.sell, OLD.cost); END;
-CREATE TRIGGER sih_history_logger AFTER UPDATE ON sih_current FOR EACH ROW WHEN OLD.sih <> NEW.sih OR OLD.sell <> NEW.sell OR OLD.cost <> NEW.cost BEGIN INSERT INTO sih_history VALUES (datetime(), cast(OLD.itemcode as int), "", cast(OLD.sih AS INT), cast(OLD.cost AS REAL), cast(OLD.sell AS REAL)); END;
+CREATE TRIGGER sih_history_logger AFTER UPDATE ON sih_current FOR EACH ROW WHEN OLD.sih <> NEW.sih OR OLD.sell <> NEW.sell OR OLD.cost <> NEW.cost BEGIN INSERT INTO sih_history VALUES (datetime(), cast(OLD.itemcode as int), '', cast(OLD.sih AS INT), cast(OLD.cost AS REAL), cast(OLD.sell AS REAL)); END;
 CREATE VIEW full_inventory_history_latest AS SELECT latest.itemcode, cost, sell FROM (SELECT itemcode, max(datetime) AS dt FROM full_inventory_history WHERE cost <> 0 AND sell <> 0 GROUP BY itemcode) latest JOIN full_inventory_history history ON latest.itemcode = history.itemcode AND latest.dt = history.datetime
 /* full_inventory_history_latest(itemcode,cost,sell) */;
 CREATE TRIGGER sih_history_desc_logger AFTER UPDATE ON sih_current FOR EACH ROW WHEN OLD.desc <> NEW.desc BEGIN INSERT INTO sih_history_desc VALUES (datetime(), cast(OLD.itemcode AS int), OLD.desc); END;
-CREATE TABLE sih_history_desc(datetime TEXT, itemcode INT, desc TEXT) STRICT;
