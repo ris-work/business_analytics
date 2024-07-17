5b5114dc7fb65b1dfdb2e5fa749db13cec63806e
root <oss@rishikeshan.com>
2024-07-17 08:16:32 +0000

    [WIP] Vendors
    FossilOrigin-Name: a8d69236047c2d5232fdcb7d02b0a21261bf0f3508a2cde05b9a56261ec9ed09

---
+++ product_vendors/cost.sql
@@ -0,0 +1,8 @@
+BEGIN TRANSACTION;
+DELETE FROM product_vendors_import;
+.import --csv cost.csv product_vendors_import
+INSERT INTO product_vendors SELECT CAST(itemcode AS INT), CAST(vendorcode AS INT), CAST(cost AS REAL), CAST(sell AS REAL) FROM product_vendors_import WHERE true ON CONFLICT DO UPDATE SET cost=CAST(excluded.cost AS REAL), sell = CAST(excluded.sell AS REAL) WHERE cost <> CAST(excluded.cost AS REAL) OR sell <> CAST(excluded.sell AS REAL);
+DELETE FROM template_import;
+COMMIT;
+VACUUM;
+ANALYZE;
---
+++ vendor_details/cost.sql
@@ -0,0 +1,8 @@
+BEGIN TRANSACTION;
+DELETE FROM vendors_import;
+.import --csv cost.csv vendors_import
+INSERT INTO vendors SELECT CAST(vendorcode AS INT), CAST(vendorname AS INT) FROM vendors_import WHERE true ON CONFLICT DO UPDATE SET vendorname=excluded.vendorname WHERE vendorname <> excluded.vendorname;
+DELETE FROM template_import;
+COMMIT;
+VACUUM;
+ANALYZE;
