0c7b1975f9deb6821661071c18d49a7355208978
root <oss@rishikeshan.com>
2024-11-27 15:11:50 +0000

    Remove irrelevant table
    FossilOrigin-Name: f30e1b1cf8dfdf20c74692134af72520fb7873f8e108673ebca4924745d441af

--- dumpanalytics.php
+++ dumpanalytics.php
@@ -241,31 +241,9 @@ echo "<script>var list = ".json_encode($response)."; list_p = JSON.parse((list))
 </fieldset>
 </details>
 <div id="printer"></div>
-<table class="named">
-<tr>
-<td>Name</td>
-<td><?php echo $response->PLU_DESC; ?></td>
-</tr>
-<tr>
-<td>Code</td>
-<td><?php echo $response->PLU_CODE; ?></td>
-</tr>
-<tr>
-<td>Selling price</td>
-<td><?php echo $response->PLU_SELL; ?></td>
-</tr>
-<tr>
-<td>SIH</td>
-<td><?php echo $response->SIH; ?></td>
-</tr>
-</table>
 <details>
 <summary>More details</summary>
 <table class="others">
-<tr>
-<th scope="col">Field</th>
-<th scope="col">Value</th>
-</tr>
 <?php /*foreach($response as $field=>$value) { ?>
 <tr>
 <td><?php echo $field; ?></td>
