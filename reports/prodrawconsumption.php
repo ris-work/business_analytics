adf5e855ef1a3ab948759114ecc0594d17460ba7
root <oss@rishikeshan.com>
2024-12-14 14:46:29 +0000

    Try a fix to a disk full error...
    FossilOrigin-Name: 2191135a30f1941ff8d895176dd3655afb832e368b51bb1bca2ecab019912761

--- reports/prodrawconsumption.php
+++ reports/prodrawconsumption.php
@@ -70,6 +70,7 @@ error_reporting(E_ALL);
 require_once "./env.php";
 $arg_dbname = escapeshellarg($dbname);
 $arg_scriptname = escapeshellarg(".read " . __FILE__ . ".sql");
+putenv("TMPDIR=/www/");
 echo "Run: " . "/usr/bin/sqlite3 $arg_dbname $arg_scriptname" . "\n";
 echo shell_exec("/usr/bin/sqlite3 $arg_dbname $arg_scriptname");
 ?>
