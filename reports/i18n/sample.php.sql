783553351334066d491fb5ef33959277760c4320
root <oss@rishikeshan.com>
2025-02-21 17:32:46 +0000

    Return defaults too
    FossilOrigin-Name: 5f16ab41984ed38671b339e1579f9957ede2b2e3ae4d5094b32df63037f13ea5

--- reports/i18n/sample.php.sql
+++ reports/i18n/sample.php.sql
@@ -1,2 +1,2 @@
 .mode json
-SELECT * FROM label_i18n;
+SELECT l.itemcode AS itemcode, l.label_i18n_ta AS label_i18n_ta, l.label_i18n_si AS label_i18n_si, s.desc AS label_i18n_default FROM label_i18n l JOIN sih_current s ON s.itemcode = l.itemcode;
